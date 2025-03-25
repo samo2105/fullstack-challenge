16b3fbce5a237c6dcc6a8f053f8bc7be15029fa5
samo2105 <samuel.cea95@gmail.com>
2025-03-25 12:06:22 -0300

    Booking tests and schema updates

--- server/lib/peek_web/schema/schema.ex
+++ server/lib/peek_web/schema/schema.ex
@@ -13,9 +13,22 @@ defmodule PeekWeb.Schema.Schema do
   # Queries
   #
   query do
-    @desc "Get a list of events"
+    @desc "Get a list of events and their bookings"
     field :events, list_of(:event) do
       resolve(&EventResolver.events/3)
+    end
+  end
+
+  #
+  # Mutations
+  #
+  mutation do
+    field :create_booking, :booking do
+      arg :event_id, non_null(:id)
+      arg :first_name, non_null(:string)
+      arg :last_name, non_null(:string)
+
+      resolve(&BookingResolver.create_booking/3)
     end
   end
 
@@ -27,16 +40,17 @@ defmodule PeekWeb.Schema.Schema do
     field :start, non_null(:naive_datetime)
     field :duration, non_null(:integer)
     field :title, non_null(:string)
-
-    field :bookings, non_null(list_of(non_null(:booking))), resolve: &BookingResolver.get_bookings/3
+    field :bookings, list_of(:booking)
   end
 
   #
   # Booking Object
   #
   object :booking do
+    field :id, non_null(:id)
     field :first_name, non_null(:string)
     field :last_name, non_null(:string)
+    field :event_id, non_null(:id)
   end
 
 end
